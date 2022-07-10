cfe5848a5bca18ba89eb59e2ff651120ccdc9894
omarqr0 <pleess30@gmail.com>
2022-07-10 22:29:39 +0300

    create a main page controller, template, and test

--- src/main/java/com/example/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/example/tacocloud/TacoCloudApplication.java
@@ -2,6 +2,9 @@ package com.example.tacocloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@Controller
 
 @SpringBootApplication
 public class TacoCloudApplication {
