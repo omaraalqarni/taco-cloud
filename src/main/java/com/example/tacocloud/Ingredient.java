73e813b1fe527d3c4fa59d3cb7d5f8a6c356215c
omarqr0 <pleess30@gmail.com>
2022-07-16 17:05:37 +0300

    WORK

---
+++ src/main/java/com/example/tacocloud/Ingredient.java
@@ -0,0 +1,17 @@
+package com.example.tacocloud;
+
+
+import lombok.Data;
+
+import java.lang.reflect.Type;
+
+@Data
+public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type{
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
---
+++ src/main/java/com/example/tacocloud/Ingredients.java
@@ -0,0 +1,5 @@
+package com.example.tacocloud;
+
+
+public enum Ingredients {
+}
